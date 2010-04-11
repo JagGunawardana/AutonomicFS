d38b464611a5651f278dad4b3ec2788f4309853c
Jag Gunawardana <jag.gunawardana@gmail.com>
2010-04-11 11:11:36 +0100

    Mid add for multiple threads.

--- NameServer/servicerequest.cpp
+++ NameServer/servicerequest.cpp
@@ -10,11 +10,10 @@ ServiceRequest::ServiceRequest(xmlrpc::Server* srv, QList<xmlrpc::Variant> param
 
 void ServiceRequest::run() {
 	QVariant ret_val = Service_RequestFile(parameters[0]);
-	srv->sendReturnValue( requestId, ret_val.toByteArray());  // !!!
+	srv->sendReturnValue( requestId, ret_val.toByteArray());
 }
 
 QVariant ServiceRequest::Service_RequestFile(QVariant file_name) {
-
 	QFile file(file_name.toString());
 	if (!file.exists()) {
 		qFatal("Cant find Nameserver.");
